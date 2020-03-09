e6f8a3047f4ccc87805b8c3a8018076aea23589c
Marde101 <martti.98@hotmail.com>
2020-03-09 16:57:55 +0200

    background added

---
+++ desktop/src/fi/tuni/game/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package fi.tuni.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fi.tuni.game.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+
+		// windows size is (pixels * tiles)
+        config.width = 1280;
+        config.height = 640;
+		new LwjglApplication(new Main(), config);
+	}
+}
