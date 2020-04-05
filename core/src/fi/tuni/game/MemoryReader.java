347792ecd2818f92bb6abbb0f369a872469a59c5
Marde101 <martti.98@hotmail.com>
2020-04-05 16:32:12 +0300

    memory funtions added

--- core/src/fi/tuni/game/MemoryReader.java
+++ core/src/fi/tuni/game/MemoryReader.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.Preferences;
 public class MemoryReader {
 
     static public void readBalance(Balance x) {
-        Preferences prefs = Gdx.app.getPreferences("MyPreferences.txt");
+        Preferences prefs = Gdx.app.getPreferences("MyPreferences.xml");
         int value = prefs.getInteger(x.getKey());
         x.setValue(value);
     }
--- core/src/fi/tuni/game/MemoryWriter.java
+++ core/src/fi/tuni/game/MemoryWriter.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.Preferences;
 public class MemoryWriter {
 
     static public void writeBalance(String key, int value) {
-        Preferences prefs = Gdx.app.getPreferences("MyPreferences.txt");
+        Preferences prefs = Gdx.app.getPreferences("MyPreferences.xml");
         prefs.putInteger(key, value);
         prefs.flush();
     }
